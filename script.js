c2ee4fc9330580e43b1507b2a973474fafcb9483
notolyte <mina37s.sumomo@gmail.com>
2023-08-14 11:48:04 +0900

    デザイン等

--- script.js
+++ script.js
@@ -13,21 +13,32 @@ document.getElementById("searchbutton").addEventListener("click", () => {
 function search(keyword) {
     const results = document.getElementById("result");
     const history = document.getElementById("history");
-    history.innerHTML = results.innerHTML + history.innerHTML;
     results.innerHTML = "";
+    document.querySelectorAll("#history div").forEach(item => {
+        item.classList.remove("tmp_none");
+    });
     entries.words.forEach(word => {
         if (word["entry"]["form"] == keyword) {
             const newResult = document.createElement("div");
             newResult.classList.add("table");
             let resultHTML = "";
+            const newHistory = document.createElement("div");
+            newHistory.classList.add("table");
+            let newHistoryHTML = "";
             resultHTML += "<h3>" + keyword + "</h3>";
+            newHistoryHTML += "<h3>" + keyword + "</h3>";
             resultHTML += "<h3>語義</h3>"
             word["translations"].forEach(definition => {
-                resultHTML += "<p>" + definition["forms"][0] + "</p>";
+                const thisDef = definition["forms"][0].replaceAll(",", "，");
+                resultHTML += "<p>" + thisDef + "</p>";
+                newHistoryHTML += "<p>" + thisDef + "</p>";
             })
             resultHTML += "<h3>発音</h3><p>" + word["contents"][0]["text"] + "</p>";
             newResult.innerHTML = resultHTML;
+            newHistory.innerHTML = newHistoryHTML;
+            newHistory.classList.add("tmp_none");
             results.appendChild(newResult);
+            history.prepend(newHistory);
         }
     })
 }

78e0d325078ca7b2d982ab0e365cfe77cbf4d934
notolyte <mina37s.sumomo@gmail.com>
2023-08-14 15:03:36 +0900

    逆引きを追加

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let entries;
+let entries, entries2;
 async function readFile() {
     const requestURL =
         "https://raw.githubusercontent.com/notolyte/okinawago_dic/main/okinawa_hondo.json";
@@ -6,6 +6,10 @@ async function readFile() {
 
     const response = await fetch(request);
     entries = await response.json();
+    const requestURL2 = "https://raw.githubusercontent.com/notolyte/okinawago_dic/main/hondo_okinawa.json";
+    const request2 = new Request(requestURL2);
+    const response2 = await fetch(request2);
+    entries2 = await response2.json();
 }
 document.getElementById("searchbutton").addEventListener("click", () => {
     search(document.forms.search.field.value);
@@ -22,11 +26,31 @@ function search(keyword) {
             results.appendChild(drawCard(word["entry"]["id"]));
             history.prepend(drawCard(word["entry"]["id"]));
         }
-    })
+    });
+    entries2.words.forEach(word => {
+        if (word["entry"]["form"] == keyword) {
+            results.appendChild(drawCard2(word["entry"]["id"]));
+            history.prepend(drawCard2(word["entry"]["id"]));
+        }
+    });
 }
 function drawCard(id) {
     const newCard = document.createElement("div");
     const word = entries["words"][id - 1];
+    newCard.innerHTML
+        = "<h3 class='midasi'>" + word["contents"][0]["text"] + "</h3>"
+        + "<p class='midasi'>" + word["translations"][0]["title"] + "</p>";
+    word["translations"].forEach(definition => {
+        const thisDef = definition["forms"][0].replaceAll(",", "，");
+        newCard.innerHTML += "<p>" + thisDef + "</p>";
+    })
+    newCard.classList.add("table");
+    newCard.classList.add("now");
+    return newCard;
+}
+function drawCard2(id) {
+    const newCard = document.createElement("div");
+    const word = entries2["words"][id - 1];
     newCard.innerHTML
         = "<h3 class='midasi'>" + word["contents"][0]["text"] + "</h3>"
         + "<p class='midasi'>" + word["translations"][0]["title"] + "</p>";
